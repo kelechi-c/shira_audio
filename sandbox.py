506c392fee85ef716e9a89766faa93196be58832
Tensor <narutokelechi@gmail.com>
2024-10-08 00:03:34 +0100

    [modify] sandbox code, project toml file

--- sandbox.py
+++ sandbox.py
@@ -1,7 +1,7 @@
 """
 testing ground :)
 """
-
+import torch
 from datasets import load_dataset
 from transformers import ClapModel, ClapProcessor
 
@@ -13,7 +13,6 @@ audio_sample = librispeech_dummy[0]
 model = ClapModel.from_pretrained("laion/larger_clap_music_and_speech").to(0)
 processor = ClapProcessor.from_pretrained("laion/larger_clap_music_and_speech")
 
-inputs = processor(
-    audios=audio_sample["audio"]["array"], return_tensors="pt").to(0)
+inputs = processor(audios=audio_sample["audio"]["array"], return_tensors="pt").to(0)
 
 audio_embed = model.get_audio_features(**inputs)
