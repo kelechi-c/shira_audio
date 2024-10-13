bbcd7520b098718fbd76bfc6642b604d32668868
Tensor <narutokelechi@gmail.com>
2024-10-13 05:00:58 +0100

    [modify] add imports for namespace trimming

--- src/wavsearch/__init__.py
+++ src/wavsearch/__init__.py
@@ -1,4 +1,5 @@
-from .utils import read_audio, audiofile_crawler
+from .utils import read_audio, audiofile_crawler, latency, mp3_to_wav
+from .audio_search import AudioEmbedding, AudioSearch, load_models
 
-def breath():
-    print('wavsearch: a neural audio retrieval library')
+def echo_shira():
+    print('shira: a neural audio retrieval library')
