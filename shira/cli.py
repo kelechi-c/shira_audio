4d66b612394a96e0007b86b134d63c94ea86fa33
Tensor <narutokelechi@gmail.com>
2024-10-16 04:11:03 +0100

    [modify] add cli functionality with click, update dependecies, v0.1.1 testing

---
+++ shira/cli.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import click
+from .audio_search import AudioEmbedding, AudioSearch
+from .utils import audiofile_crawler
+
+
+@click.command()
+@click.option('-d', '--dir', default=lambda: Path.home(), type=str)
+def check_files(dir):
+    _, f_count = audiofile_crawler(dir)
+    click.echo('file crawling complete')
+
+
+@click.command()
+@click.option("-t", "--textquery", required=True, type=str, help="text description for audio retrieval")
+@click.option("-d", "--dir", default=lambda: Path.home(), type=str, help='target directory')
+def text_search(textquery: str):
+    
+    embedder = AudioEmbedding(data_path='.') # init embedder class
+    
+    audio_data_embeds = embedder.index_files() # create embeddings and index audio files
+    neural_search = AudioSearch() # init semantic search class
+
+    # get k similar audio w/probability scores pairs
+    matching_samples, scores = neural_search.text_search(textquery, audio_data_embeds, k_count=5)
+
+    top_sample = matching_samples[0]['audio']['path'] # get file path for top sample
+
+    click.echo(f"text query {textquery}")
+    click.echo("...........")
+    click.echo(f"search result #1 {top_sample}")
